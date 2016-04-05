91d50e8cb26c479761418e5d07d8ff2a35c5227f
MarinaKorniychuk <kornij4uk@gmail.com>
2016-04-05 23:15:58 +0300

    Modifeid fields' structure.

--- MineSweeper.cpp
+++ MineSweeper.cpp
@@ -41,7 +41,7 @@ public:
 	state_of_cell value;					/* 0 - 8 mines around. 9 if mine. */
 	int mine;
 	bool open;								/* state of cell during work of DFS. */
-	int dx[8] = { 0,0,1,-1,1,1,-1,-1 };
+	int dx[8] = { 0,0,1,-1,1,1,-1,-1 };		/* for DFS */
 	int dy[8] = { 1,-1,1,1,0,-1,-1,0 };
 	cl_cell()
 	{
@@ -51,18 +51,18 @@ public:
 		open = false;
 	};
 
-	void change_value(state_of_cell val)
+	void change_value(state_of_cell val)	
 	{
 		value = val;
 	}
 
-	void change_state(state_of_cell st)
+	void change_state(state_of_cell st)		/* when Right button pressed. */
 	{
 		state = st;
 		sprite.setTextureRect(IntRect(state * 32, 0, 32, 32));
 	}
 
-	void change_texture()				/* "open" the cell */
+	void change_texture()					/* "open" the cell. */
 	{
 		state = value;
 		sprite.setTextureRect(IntRect(state * 32, 0, 32, 32));
@@ -74,21 +74,54 @@ public:
 class field
 {
 public:
-	int width;
-	int height;
+	int width;								/* width of field in pixels. */
+	int height;								/* height of field in pixels. */
 	int amount_of_mines; 
-	bool token;
-	field (int w, int h, int am) 
+	int n_x;								/* width of field in cells. */
+	int n_y;								/* height of field in cells. */
+	bool token;								/* to call generator. */
+	bool game;								/* to finish the game. */
+	cl_cell **Cell;							/* array of cells. */
+	Texture texture;
+	field() 
 	{
-		width = w;
-		height = h;
-		amount_of_mines = am;
-		token = true;
+		width = WIDTH;					
+		height = HEIGHT;					
+		amount_of_mines = MINES;
+		token = true;						
+		game = true;						
 	}
 
-	/*place mines on the field and count value for each cell. */
+	/* Create a two-dimensional array of type cl_cell. */
 
-	void generator(cl_cell** cell, int i, int j, int x, int y)
+	void create_field()
+	{
+		Cell = new cl_cell *[n_y];
+
+		for (int i = 0; i < n_y; i++)
+		{
+			Cell[i] = new cl_cell[n_x];
+		}
+	}
+
+	/* Setting texture to each cell. */
+
+	void set_texture()
+	{
+		for (int i = 0; i < n_y; i++)
+		{
+			for (int j = 0; j < n_x; j++)
+			{
+				Cell[i][j].sprite.setPosition(j * 32, i * 32);
+				Cell[i][j].sprite.setTexture(texture);
+				Cell[i][j].sprite.setTextureRect(IntRect(Cell[i][j].state * 32, 0, 32, 32));
+			}
+		}
+	}
+
+	/* Place mines on the field and count value for each cell. */
+
+	void generator(int x, int y)
 	{
 
 		/* place mines and change value for cell that was chosen. */
@@ -96,50 +129,50 @@ public:
 
 		/* at first change value of near cells to avoid the posibility of placing mines in them */
 
-		if ((y > 0) && (y + 1 < i) && (x > 0) && (x + 1 < j))	/* if not a borden cell was chosen. */
+		if ((y > 0) && (y + 1 < n_y) && (x > 0) && (x + 1 < n_x))	/* if not a borden cell was chosen. */
 		{															
 			for (int k = y - 1; k <= y + 1; k++)					
 			{														
 				for (int m = x - 1; m <= x + 1; m++)					
 				{														
-					cell[k][m].value = N1;								
+					Cell[k][m].value = N1;								
 				}													
 			}											
 		}
 		else													/* if a borden cell was chosen. */
 		{
-			cell[y][x].value = N1;
+			Cell[y][x].value = N1;
 			if ((y - 1 >= 0) && (x - 1 >= 0))
 			{
-				cell[y - 1][x - 1].value = N1;
+				Cell[y - 1][x - 1].value = N1;
 			}
 			if (y - 1 >= 0)
 			{
-				cell[y - 1][x].value = N1;
+				Cell[y - 1][x].value = N1;
 			}
-			if ((y - 1 >= 0) && (x + 1 < j))
+			if ((y - 1 >= 0) && (x + 1 < n_x))
 			{
-				cell[y - 1][x + 1].value = N1;
+				Cell[y - 1][x + 1].value = N1;
 			}
-			if (x + 1 < j)
+			if (x + 1 < n_x)
 			{
-				cell[y][x + 1].value = N1;
+				Cell[y][x + 1].value = N1;
 			}
 			if (x - 1 >= 0)
 			{
-				cell[y][x - 1].value = N1;
+				Cell[y][x - 1].value = N1;
 			}
-			if ((y + 1 < i) && (x - 1 >= 0))
+			if ((y + 1 < n_y) && (x - 1 >= 0))
 			{
-				cell[y + 1][x - 1].value = N1;
+				Cell[y + 1][x - 1].value = N1;
 			}
-			if (y + 1 < i)
+			if (y + 1 < n_y)
 			{
-				cell[y + 1][x].value = N1;
+				Cell[y + 1][x].value = N1;
 			}
-			if ((y + 1 < i) && (x + 1 < j))
+			if ((y + 1 < n_y) && (x + 1 < n_x))
 			{
-				cell[y + 1][x + 1].value = N1;
+				Cell[y + 1][x + 1].value = N1;
 			}
 
 
@@ -154,68 +187,68 @@ public:
 			std::srand(std::time(NULL));
 			do
 			{
-				new_i = (std::rand() % i);
-				new_j = (std::rand() % j);
-			} while ((cell[new_i][new_j].value == MINE) || (cell[new_i][new_j].value == N1));
-			cell[new_i][new_j].value = MINE;
+				new_i = (std::rand() % n_y);
+				new_j = (std::rand() % n_x);
+			} while ((Cell[new_i][new_j].value == MINE) || (Cell[new_i][new_j].value == N1));
+			Cell[new_i][new_j].value = MINE;
 		}
 		
-		for (int k = 0; k < i; k++)
+		for (int k = 0; k < n_y; k++)
 		{
-			for (int m = 0; m < j; m++)
+			for (int m = 0; m < n_x; m++)
 			{
-				if (cell[k][m].value != MINE)
-				cell[k][m].value = N0;
+				if (Cell[k][m].value != MINE)
+				Cell[k][m].value = N0;
 			}
 		}
 		
 		/* define the amount of mines near for each cell. */
 
-		for (int k = 0; k < i; k++)
+		for (int k = 0; k < n_y; k++)
 		{
-			for (int m = 0; m < j; m++)
+			for (int m = 0; m < n_x; m++)
 			{
-				if (cell[k][m].value == MINE)
+				if (Cell[k][m].value == MINE)
 				{
 					if ((k - 1 >= 0) && (m - 1 >= 0))
 					{
-						if (cell[k - 1][m - 1].value != MINE)
-							cell[k - 1][m - 1].mine++;
+						if (Cell[k - 1][m - 1].value != MINE)
+							Cell[k - 1][m - 1].mine++;
 					}
 					if (k - 1 >= 0)
 					{
-						if (cell[k - 1][m].value != MINE)
-							cell[k - 1][m].mine++;
+						if (Cell[k - 1][m].value != MINE)
+							Cell[k - 1][m].mine++;
 					}
-					if ((k - 1 >= 0) && (m + 1 < j))
+					if ((k - 1 >= 0) && (m + 1 < n_x))
 					{
-						if (cell[k - 1][m + 1].value != MINE)
-							cell[k - 1][m + 1].mine++;
+						if (Cell[k - 1][m + 1].value != MINE)
+							Cell[k - 1][m + 1].mine++;
 					}
-					if (m + 1 < j)
+					if (m + 1 < n_x)
 					{
-						if (cell[k][m + 1].value != MINE)
-							cell[k][m + 1].mine++;
+						if (Cell[k][m + 1].value != MINE)
+							Cell[k][m + 1].mine++;
 					}
 					if (m - 1 >= 0)
 					{
-						if (cell[k][m - 1].value != MINE)
-							cell[k][m - 1].mine++;
+						if (Cell[k][m - 1].value != MINE)
+							Cell[k][m - 1].mine++;
 					}
-					if ((k + 1 < i) && (m - 1 >= 0))
+					if ((k + 1 < n_y) && (m - 1 >= 0))
 					{
-						if (cell[k + 1][m - 1].value != MINE)
-							cell[k + 1][m - 1].mine++;
+						if (Cell[k + 1][m - 1].value != MINE)
+							Cell[k + 1][m - 1].mine++;
 					}
-					if (k + 1 < i)
+					if (k + 1 < n_y)
 					{
-						if (cell[k + 1][m].value != MINE)
-							cell[k + 1][m].mine++;
+						if (Cell[k + 1][m].value != MINE)
+							Cell[k + 1][m].mine++;
 					}
-					if ((k + 1 < i) && (m + 1 < j))
+					if ((k + 1 < n_y) && (m + 1 < n_x))
 					{
-						if (cell[k + 1][m + 1].value != MINE)
-							cell[k + 1][m + 1].mine++;
+						if (Cell[k + 1][m + 1].value != MINE)
+							Cell[k + 1][m + 1].mine++;
 					}
 				}
 			}
@@ -223,35 +256,35 @@ public:
 
 		/* change the value of each cell according to defines amount of mines near. */
 
-		for (int k = 0; k < i; k++)
+		for (int k = 0; k < n_y; k++)
 		{
-			for (int m = 0; m < j; m++)
+			for (int m = 0; m < n_x; m++)
 			{
-				switch (cell[k][m].mine)
+				switch (Cell[k][m].mine)
 				{
 				case 1:
-					cell[k][m].value = N1;
+					Cell[k][m].value = N1;
 					break;
 				case 2:
-					cell[k][m].value = N2;
+					Cell[k][m].value = N2;
 					break;
 				case 3:
-					cell[k][m].value = N3;
+					Cell[k][m].value = N3;
 					break;
 				case 4:
-					cell[k][m].value = N4;
+					Cell[k][m].value = N4;
 					break;
 				case 5:
-					cell[k][m].value = N5;
+					Cell[k][m].value = N5;
 					break;
 				case 6:
-					cell[k][m].value = N6;
+					Cell[k][m].value = N6;
 					break;
 				case 7:
-					cell[k][m].value = N7;
+					Cell[k][m].value = N7;
 					break;
 				case 8:
-					cell[k][m].value = N8;
+					Cell[k][m].value = N8;
 					break;
 				default:
 					break;
@@ -261,36 +294,127 @@ public:
 
 		/* if we need to show field after generation. */
 
-		/* change texture. */
-
-		/*for (int k = 0; k < i; k++)
+		/*for (int k = 0; k < n_y; k++)
 		{
-			for (int m = 0; m < j; m++)
+			for (int m = 0; m < n_x; m++)
 			{
 				cell[k][m].change_texture();
 			}
 		}*/
+
+		/* call dfs dor opennig empty cell. */
+
+		dfs(x, y);
+
+		/* change value of token to not call generator any more. */
+
+		token = false;
 	}
 
-	/* --------------------------------------------------------------------------------------------------------------------------- */
+	/* Depth-First Search -- to open cell without mines with recursion. */
 
-	/*Depth-First Search -- to open cell without mines with recursion. */
-
-	void dfs(cl_cell **cell, int x, int y)
+	void dfs(int x, int y)
 	{
 		const int n_x = WIDTH / CELL_SIZE;
 		const int n_y = HEIGHT / CELL_SIZE;
 
-		if (cell[y][x].open == true)
+		if (Cell[y][x].open == true)
 			return;
-		cell[y][x].open = true;
-		cell[y][x].change_texture();
-		if (cell[y][x].value == 0)
+		Cell[y][x].open = true;
+		Cell[y][x].change_texture();
+		if (Cell[y][x].value == 0)
 		{
 			for (int i = 0; i < 8; i++)
 			{
-				if ((x + cell[y][x].dx[i] >= 0) && (x + cell[y][x].dx[i] < n_x) && (y + cell[y][x].dy[i] >= 0) && (y + cell[y][x].dy[i] < n_y))
-					dfs(cell, x + cell[y][x].dx[i], y + cell[y][x].dy[i]);
+				if ((x + Cell[y][x].dx[i] >= 0) && (x + Cell[y][x].dx[i] < n_x) && (y + Cell[y][x].dy[i] >= 0) && (y + Cell[y][x].dy[i] < n_y))
+					dfs(x + Cell[y][x].dx[i], y + Cell[y][x].dy[i]);
+			}
+		}
+	}
+
+	/* Actions for Right button pressed*/
+
+	void Right_Button(int x, int y)
+	{
+		/* place FLAG/QUESTION/CLOSED. */
+
+		while (1)
+		{
+			if (Cell[y][x].state == CLOSED)
+			{
+				Cell[y][x].change_state(FLAG);
+				break;
+			}
+			if (Cell[y][x].state == FLAG)
+			{
+				Cell[y][x].change_state(MARK);
+				break;
+			}
+			if (Cell[y][x].state == MARK)
+			{
+				Cell[y][x].change_state(CLOSED);
+				break;
+			}
+			break;
+		}
+	}
+
+	/* Actions for Left button pressed*/
+
+	void Left_Button(int x, int y)
+	{
+
+		if ((Cell[y][x].value >= N1) && (Cell[y][x].value <= N8))
+		{
+			int count_for_flag = 0;
+			for (int i = 0; i < 8; i++)
+			{
+				int new_x = x + Cell[y][x].dx[i];
+				int new_y = y + Cell[y][x].dy[i];
+				if ((new_x >= 0) && (new_x < n_x) && (new_y >= 0) && (new_y < n_y))
+				{
+					if (Cell[new_y][new_x].state == FLAG)
+						count_for_flag++;
+				}
+			}
+			std::cout << count_for_flag;
+			if (count_for_flag == Cell[y][x].value)
+			{
+				for (int i = 0; i < 8; i++)
+				{
+					int new_x = x + Cell[y][x].dx[i];
+					int new_y = y + Cell[y][x].dy[i];
+					if ((new_x >= 0) && (new_x < n_x) && (new_y >= 0) && (new_y < n_y))
+					{
+						if (Cell[new_y][new_x].state != FLAG)
+						{
+							if (Cell[new_y][new_x].value == N0)
+								dfs(new_x, new_y);
+							Cell[new_y][new_x].change_texture();
+						}
+					}
+				}
+			}
+		}
+
+		/* call generator if our field is empty. */
+
+		if (token == true)
+		{
+			generator(x, y);
+
+		}
+		else
+		{
+			if (Cell[y][x].value == N0)
+				dfs(x, y);
+			else
+				Cell[y][x].change_texture();
+			if (Cell[y][x].value == MINE)
+			{
+				std::cout << "Game Over";
+				game = false;
+				//game_field_w.close();
 			}
 		}
 	}
@@ -301,12 +425,11 @@ const std::string texture = "images/Texture.png";
 
 int main()
 {
-	
 	int amount_of_mines = MINES;
 
 	/* create the instance of class "field" with specified arguments */
 
-	field game_field(WIDTH, HEIGHT, amount_of_mines);
+	field game_field;
 
 	/* create the window with arguments from class. */
 
@@ -314,34 +437,22 @@ int main()
 
 	/* loading the texture for sprite. */ 
 
-	Texture game_texture;
-	game_texture.loadFromFile(texture);
+	game_field.texture.loadFromFile(texture);
 
 	/* define the width and the height of the field in cells. */
 
-	const int n_x = WIDTH / CELL_SIZE;		
-	const int n_y = HEIGHT / CELL_SIZE;	
+	game_field.n_x = WIDTH / CELL_SIZE;		
+	game_field.n_y = HEIGHT / CELL_SIZE;	
 
 	/* create a two-dimensional array of type cl_cell. */
 
-	cl_cell **Cell = new cl_cell *[n_y];
+	game_field.create_field();
 
-	for (int i = 0; i < n_y; i++)
-	{
-		Cell[i] = new cl_cell[n_x];
-	}
+	/* setting texture to each sprite. */
 
-	/* set initial texture to each cell of field. */
-
-	for (int i = 0; i < n_y; i++)
-	{
-		for (int j = 0; j < n_x; j++)
-		{
-			Cell[i][j].sprite.setPosition(j * 32, i * 32);
-			Cell[i][j].sprite.setTexture(game_texture);
-			Cell[i][j].sprite.setTextureRect(IntRect(Cell[i][j].state * 32, 0, 32, 32));
-		}
-	}
+	game_field.set_texture();
+	
+	
 
 	/* main cycle. */
 
@@ -356,80 +467,38 @@ int main()
 			case Event::Closed:
 				game_field_w.close();
 				break;
+
 			case Event::MouseButtonPressed:
 
 				int x = Mouse::getPosition(game_field_w).x / CELL_SIZE;
 				int y = Mouse::getPosition(game_field_w).y / CELL_SIZE;
 
-				/* place FLAG/QUESTION. */
-
 				if (event.mouseButton.button == sf::Mouse::Right)
-				{
-					std::cout << Cell[y][x].value << std::endl;
-					while(1)
-					{
-						if (Cell[y][x].state == CLOSED)
-						{
-							Cell[y][x].change_state(FLAG);
-							break;
-						}
-						if (Cell[y][x].state == FLAG)
-						{
-							Cell[y][x].change_state(MARK);
-							break;
-						}
-						if (Cell[y][x].state == MARK)
-						{
-							Cell[y][x].change_state(CLOSED);
-							break;
-						}
-						break;
-					}
-				}
-
+				
+					game_field.Right_Button(x, y);
 
 				if (event.mouseButton.button == sf::Mouse::Left)
-				{
-					int count = 0;
-
-					/* call generator if our field is empty. */
-
-					if (game_field.token == true) 
-					{
-						game_field.generator(Cell, n_y, n_x, x, y);
-						game_field.dfs(Cell, x, y);
-						game_field.token = false;
-					}
-					else
-					{
-						if (Cell[y][x].value == N0)
-							game_field.dfs(Cell, x, y);
-						else
-						Cell[y][x].change_texture();
-						if (Cell[y][x].value == MINE)
-						{
-							std::cout << "Game Over";
-							game_field_w.close();
-						}
-					}
-				}
+		
+					game_field.Left_Button(x, y);
 				
 				break;
-			
 			}
 		}
 
 
 		game_field_w.clear();
 
-		for (int i = 0; i < n_y; i++)
+		for (int i = 0; i < game_field.n_y; i++)
 		{
-			for (int j = 0; j < n_x; j++)
+			for (int j = 0; j < game_field.n_x; j++)
 			{
-				game_field_w.draw(Cell[i][j].sprite);
+				game_field_w.draw(game_field.Cell[i][j].sprite);
 			}
 		}
 		game_field_w.display();
+
+		if (game_field.game == false)
+			game_field_w.close();
 	}
 
 	getchar();
